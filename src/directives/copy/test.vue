8255f1b85eaedadd145f25fd91c5f4eb13bc92f7
baozhangjie <baozhangjie@xiaohongshu.com>
2024-07-01 16:08:10 +0800

    feat: 增加copy自定义指令，安装copy-to-clipboard库进行复制操作

---
+++ src/directives/copy/test.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- copy -->
+    <Button v-copy="value"> 复制 </Button>
+    <input type="text" v-model="value"/>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const value = ref('')
+</script>
---
+++ src/directives/loading/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- loading -->
+    <Button @click="isLoading = !isLoading"> 加载 </Button>
+    当前状态： {{ isLoading }}
+    <div class="directive-loading-test" v-loading="isLoading">我是内容</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const isLoading = ref(false)
+</script>
+
+<style scope>
+.directive-loading-test {
+  width: 200px;
+  height: 100px;
+  border-radius: 5px;
+  border: 1px solid black;
+  margin-top: 10px;
+}
+</style>
